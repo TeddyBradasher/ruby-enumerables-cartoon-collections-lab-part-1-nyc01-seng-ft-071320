e4bc82ee9fbfaba783af961ca635771d9f887225
Teddy Bradsher <teddybradsher@gmail.com>
2020-07-09 02:13:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def greet_characters(array)
   puts "Good morning #{element}!"
   
   
-  
+end
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
